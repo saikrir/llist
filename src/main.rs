339ff88a469c0242a9300f5b00f3f8571c672c68
Sai Katterishetty <rao.saikrishna@gmail.com>
2026-01-15 22:11:12 -0600

    Added more functionality

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,58 @@
-use std::fmt::Debug;
+use std::{fmt::Debug, ops::Deref};
+
+struct ListIter<'a, T: PartialEq + Debug> {
+    next: Option<&'a Node<T>>,
+}
+
+impl<'a, T: PartialEq + Debug> Iterator for ListIter<'a, T> {
+    type Item = &'a T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.map(|node| {
+            self.next = node.next.as_deref();
+            &node.val
+        })
+    }
+}
+
+struct ListIterMut<'a, T: PartialEq + Debug> {
+    next: Option<&'a mut Node<T>>,
+}
+
+impl<'a, T: PartialEq + Debug> Iterator for ListIterMut<'a, T> {
+    type Item = &'a mut T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.next.take().map(|v| {
+            self.next = v.next.as_deref_mut();
+            &mut v.val
+        })
+    }
+}
+
+struct IntoListIter<T: PartialEq + Debug>(LinkedList<T>);
+
+impl<T: PartialEq + Debug> Iterator for IntoListIter<T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        self.0.pop()
+    }
+}
 
 #[derive(Debug)]
 struct LinkedList<T: PartialEq + Debug> {
     head: Option<Box<Node<T>>>,
+}
+
+impl<T: PartialEq + Debug> IntoIterator for LinkedList<T> {
+    type Item = T;
+
+    type IntoIter = IntoListIter<T>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        IntoListIter(self)
+    }
 }
 
 impl<T: PartialEq + Debug> LinkedList<T> {
@@ -41,6 +91,22 @@ impl<T: PartialEq + Debug> LinkedList<T> {
             None => None,
         }
     }
+
+    fn peek_mut(&mut self) -> Option<&mut T> {
+        self.head.as_mut().map(|node| &mut node.val)
+    }
+
+    fn iter(&self) -> ListIter<'_, T> {
+        ListIter {
+            next: self.head.as_ref().map(|node| node.deref()),
+        }
+    }
+
+    fn iter_mut(&mut self) -> ListIterMut<'_, T> {
+        ListIterMut {
+            next: self.head.as_mut().map(|node| node.as_mut()),
+        }
+    }
 }
 
 #[derive(Debug)]
@@ -57,13 +123,9 @@ fn main() {
     list.push("Rao".to_owned());
     list.push("Katterishetty".to_owned());
 
-    println!("peek {:?}", list.peek());
+    for x in list.iter_mut() {
+        x.push_str("Test");
+    }
 
-    //    dbg!(&list);
-
-    println!("{:?}", list.pop());
-    println!("{:?}", list.pop());
-
-    //   dbg!(&list);
-    println!("peek2 {:?}", list.peek());
+    dbg!(list);
 }
